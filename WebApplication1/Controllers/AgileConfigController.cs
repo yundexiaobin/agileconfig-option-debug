032bfdb73db8df881c14dd6e3c532c71b420956c
dexter <dexter@wizmacau.com>
2022-09-19 10:16:21 +0800

    first commit

---
+++ WebApplication1/Controllers/AgileConfigController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AgileConfigController: ControllerBase
+{
+    public AgileConfigController(IOptions<GlobalOptions> options,IOptionsMonitor<GlobalOptions> optionsMonitor,
+        IOptionsSnapshot<GlobalOptions> optionsSnapshot)
+    {
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " IOptions: " + options.Value.OptionsTest);
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")+ " IOptionsMonitor: " + optionsMonitor.CurrentValue.OptionsTest);
+        Console.WriteLine( DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " IOptionsSnapshot: " + optionsSnapshot.Value.OptionsTest);
+    }
+
+    [HttpGet]
+    public Task TestAsync()
+    {
+        return Task.CompletedTask;
+    } 
+}
